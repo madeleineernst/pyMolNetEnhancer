eb5c8b50df4f1ca301a2cb7d6e1f7b969e506b61
Madeleine Ernst <madeleineernst@Madeleines-MacBook-Pro.local>
2019-02-22 10:51:01 +0100

    corrected init file

--- pyMolNetEnhancer/__init__.py
+++ pyMolNetEnhancer/__init__.py
@@ -1,2 +1,2 @@
 name = "pyMolNetEnhancer"
-from .calssyfire import *
+from .classyfire import unique_smiles, make_inchidic, highestscore, molfam_classes, make_classy_table, get_structure_class_entity, get_structure_class, structure_query, iupac_query, get_results, get_entity, get_chemont_node, tabular_query, sdf_query, _prevent_overwrite, run_shell_command,  run_parallel_shellcommands, run_parallel_job, get_classifications
